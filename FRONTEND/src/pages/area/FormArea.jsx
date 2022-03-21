2ed2c7b291ed3949c313690c7b7c965c4fde07d9
Moisas <mopri08@gmail.com>
2022-03-21 14:23:15 -0300

    create the post of area and logical about button new

--- FRONTEND/src/pages/area/FormArea.jsx
+++ FRONTEND/src/pages/area/FormArea.jsx
@@ -4,6 +4,8 @@ import Select from '../../components/select/MySelect'
 import CheckBox from '../../components/checkBox/MyCheckBox'
 import Button from '../../components/button/MyButton'
 import api from '../../api/api'
+import style from '../area/FormArea.module.css'
+import Message from '../../components/message/Message'
 
 function FormArea() {
 	const [id, setId] = useState('')
@@ -11,18 +13,62 @@ function FormArea() {
 	const [sector, setSector] = useState('')
 	const [actived, setActived] = useState(true)
 	const [listSector, setListSector] = useState([])
-
+	const [listArea, setListArea] = useState([])
+	const [nameButton, setNameButton] = useState('Incluir')
+	const [show, setShow] = useState('create')
+	const [message, setMessage] = useState(undefined)
+	const [type, setType] = useState(undefined)
 	useEffect(() => {
-		const allSector = async () => {
-			const result = await api.get('/sector')
-			setListSector(result.data)
-		}
 		allSector()
 	}, [])
 
+	const allSector = async () => {
+		const result = await api.get('/sector')
+		setListSector(result.data)
+		return result.data
+	}
+
+	const handleListAreas = () => {
+		setShow('list')
+	}
+
+	const handleNew = () => {
+		setId('')
+		setArea('')
+		setSector(allSector())
+		setActived(true)
+		setMessage(undefined)
+		setType(undefined)
+		setShow('create')
+	}
+
+	const submit = async (e) => {
+		e.preventDefault()
+		try {
+			const result = await api.post('/area', {
+				area,
+				sector_id: sector,
+			})
+			setListArea(result.data)
+			setType('success')
+			setMessage('Area incluída com sucesso!')
+			setTimeout(() => {
+				handleNew()
+			}, 2000)
+		} catch (error) {
+			setType('error')
+			error.response.data.error
+				? setMessage(error.response.data.error)
+				: setMessage(error.response.data.erros)
+		}
+		setTimeout(() => {
+			handleNew()
+		}, 2000)
+	}
+
 	return (
-		<div>
-			<form>
+		<>
+			<form className={style.container} onSubmit={submit}>
 				<Input
 					label='ID'
 					name='id'
@@ -39,14 +85,15 @@ function FormArea() {
 					type='text'
 					placeholder='Digite a area'
 					disable={false}
-					handleChange={(e) => setArea(e.target.value)}
+					handleChange={(e) => setArea(e.target.value.toUpperCase())}
 				/>
 				<Select
 					text='Setor'
 					name='sector'
 					value={sector}
 					handleChange={(e) => setSector(e.target.value)}
-					initial_text='Escolha um setor...'>
+					initial_text='Escolha um setor...'
+				>
 					{listSector.map((sector) => {
 						return (
 							<option key={sector.sector_id} value={sector.sector_id}>
@@ -62,9 +109,32 @@ function FormArea() {
 					checked={actived && true}
 					togleChange={(e) => setActived(e.target.value)}
 				/>
-				<Button>Incluir</Button>
+				<div className={style.buttons}>
+					<Button type='submt' height='2em' width='4em' marginTop='1em'>
+						{nameButton}
+					</Button>
+					<Button
+						type='button'
+						height='2em'
+						width='4em'
+						marginTop='1em'
+						handleClick={handleListAreas}
+					>
+						Setores
+					</Button>
+					<Button
+						type='button'
+						height='2em'
+						width='4em'
+						marginTop='1em'
+						handleClick={handleNew}
+					>
+						Novo
+					</Button>
+				</div>
 			</form>
-		</div>
+			{message !== undefined ? <Message type={type}>{message}</Message> : ''}
+		</>
 	)
 }
 
