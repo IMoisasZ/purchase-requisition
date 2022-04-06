c61a9dbaef353e9203eaa1cd05c09eb9728da86e
Moisas <mopri08@gmail.com>
2022-04-06 00:07:07 -0300

    ajustment liks and put the images about each link

--- FRONTEND/src/pages/menu/Menu.jsx
+++ FRONTEND/src/pages/menu/Menu.jsx
@@ -1,40 +1,74 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import WarehouseIcon from '@mui/icons-material/Warehouse'
+import BalanceSharpIcon from '@mui/icons-material/BalanceSharp'
+import Crop54Icon from '@mui/icons-material/Crop54'
+import AccountBoxIcon from '@mui/icons-material/AccountBox'
+import ViewInArSharpIcon from '@mui/icons-material/ViewInArSharp'
+import AdjustSharpIcon from '@mui/icons-material/AdjustSharp'
+import LocalPoliceSharpIcon from '@mui/icons-material/LocalPoliceSharp'
+import PersonIcon from '@mui/icons-material/Person'
+import SummarizeSharpIcon from '@mui/icons-material/SummarizeSharp'
+import FormatListNumberedIcon from '@mui/icons-material/FormatListNumbered'
 import Container from '../../components/MyContainer'
 import style from '../menu/Menu.module.css'
 
 function Menu() {
 	return (
-		<Container>
-			<h1 style={{ textAlign: 'center' }}>Menu Principal</h1>
+		<Container minHeight='68vh'>
+			<h1 style={{ textAlign: 'center', margin: '0' }}>Menu Principal</h1>
 			<div className={style.container}>
 				<section>
 					<ul>
 						<li>Cadastros</li>
 						<ul>
-							<Link to='/sector'>
-								<li>Setor</li>
+							<Link to='/sector' style={{ textDecoration: 'none' }}>
+								<div className={style.menu}>
+									<WarehouseIcon />
+									<li>Setor</li>
+								</div>
 							</Link>
 							<Link to='/unity'>
-								<li>Unidade</li>
+								<div className={style.menu}>
+									<BalanceSharpIcon />
+									<li>Unidade</li>
+								</div>
 							</Link>
 							<Link to='/area'>
-								<li>Area</li>
+								<div className={style.menu}>
+									<Crop54Icon />
+									<li>Area</li>
+								</div>
 							</Link>
 							<Link to='/role'>
-								<li>Perfil de Usuário</li>
+								<div className={style.menu}>
+									<AccountBoxIcon />
+									<li>Perfil de Usuário</li>
+								</div>
 							</Link>
 							<Link to='/user'>
-								<li>Usuário</li>
+								<div className={style.menu}>
+									<PersonIcon />
+									<li>Usuário</li>
+								</div>
 							</Link>
 							<Link to='/product'>
-								<li>Produto</li>
+								<div className={style.menu}>
+									<ViewInArSharpIcon />
+									<li>Produto</li>
+								</div>
 							</Link>
 							<Link to='/cost-center'>
-								<li>Centro de Custos</li>
+								<div className={style.menu}>
+									<AdjustSharpIcon />
+									<li>Centro de Custos</li>
+								</div>
 							</Link>
 							<Link to='/responsable'>
-								<li>Responsáveis</li>
+								<div className={style.menu}>
+									<LocalPoliceSharpIcon />
+									<li>Responsáveis</li>
+								</div>
 							</Link>
 						</ul>
 					</ul>
@@ -44,7 +78,10 @@ function Menu() {
 						<li>Requisição</li>
 						<ul>
 							<Link to='requisicao'>
-								<li>Criar Requisição</li>
+								<div className={style.menu}>
+									<FormatListNumberedIcon />
+									<li>Criar Requisição</li>
+								</div>
 							</Link>
 						</ul>
 					</ul>
@@ -54,7 +91,10 @@ function Menu() {
 						<li>Relatórios/Conusltas</li>
 						<ul>
 							<Link to='/consultar_requisicao'>
-								<li>Requisição</li>
+								<div className={style.menu}>
+									<SummarizeSharpIcon />
+									<li>Requisição</li>
+								</div>
 							</Link>
 						</ul>
 					</ul>
