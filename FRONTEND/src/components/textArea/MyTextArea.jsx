5365507b1e6b037014226b8eb33da17eee3cbc8a
Moisas <mopri08@gmail.com>
2022-04-07 00:26:04 -0300

    create a text area

---
+++ FRONTEND/src/components/textArea/MyTextArea.jsx
@@ -0,0 +1,37 @@
+import React, { Children } from 'react'
+
+function MyTextArea({
+	children,
+	name,
+	placeHolder,
+	handleChange,
+	text = 'Name label text area',
+	cols = '30',
+	rows = '10',
+	value,
+}) {
+	return (
+		<div
+			style={{
+				display: 'flex',
+				justifyContent: 'center',
+				flexDirection: 'column',
+				alignItems: 'center',
+			}}>
+			<label htmlFor={name}>{text}</label>
+			<textarea
+				style={{ resize: 'none', borderRadius: '0.3em' }}
+				name={name}
+				id={name}
+				cols={cols}
+				rows={rows}
+				placeholder={placeHolder}
+				onChange={handleChange}
+				value={value}>
+				{children}
+			</textarea>
+		</div>
+	)
+}
+
+export default MyTextArea
